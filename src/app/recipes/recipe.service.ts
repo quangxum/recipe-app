56764566606cdd5cf88b559c5aeffcf5af8e674d
Lauren Judge <l.m.judge@Hotmail.com>
2019-10-28 11:43:35 +1100

    fixed empty ingredients bug when fetching

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,22 +8,24 @@ import { Subject } from 'rxjs';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe('Tasty Schnitzel', 
-            'A super-tasty Schnitzel - just awesome!', 
-            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]),
-        new Recipe('Big Fat Burger',
-            'What else do I need to say?',
-            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1)
-            ])
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe('Tasty Schnitzel', 
+    //         'A super-tasty Schnitzel - just awesome!', 
+    //         'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]),
+    //     new Recipe('Big Fat Burger',
+    //         'What else do I need to say?',
+    //         'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRu68S8M3nQDbkKc341t9GlzvLT52S7MFW-vQuciR0waTcXgi2o',
+    //         [
+    //             new Ingredient('Buns', 2),
+    //             new Ingredient('Meat', 1)
+    //         ])
+    // ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) {}
 
