72cac65e67bcd3196ad042df4abc9ee6ee92de43
Lauren Judge <l.m.judge@Hotmail.com>
2019-10-31 10:11:23 +1100

    sign up form created and errors handled

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDH7nsYHVWA_xESZGKiOI_99-Py0SSBQdw',
+        {
+          email: email,
+          password: password,
+          returnSectureToken: true
+        }
+      ).pipe(catchError(errorRes => {
+        let errorMessage = 'An unknown error occurred';
+        if (!errorRes.error || !errorRes.error.error) {
+          return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+          case 'EMAIL_EXISTS':
+            errorMessage = 'This email already exists';
+        }
+        return throwError(errorMessage);
+      }));
+  }
+}
